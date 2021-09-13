5cbe35679b1de0f6a237dbe58f482476d759da8b
CemTunna <cemroot@gmail.com>
2021-09-13 23:47:52 +0300

    interfaces done

--- classesAndInterfaces/ts/interfaces.ts
+++ classesAndInterfaces/ts/interfaces.ts
@@ -1,4 +1,6 @@
 //describes the structure of objects
+//interces can only be used to structure object, type cant be used
+//interfaces has no implemention whereas abstract classes might have
 interface Human {
   age: number;
   name: string;
@@ -13,3 +15,17 @@ user = {
   },
 };
 user.sayHi('hello world');
+
+class Person implements Human {
+  age: number;
+  name: string;
+  constructor(age: number, name: string) {
+    this.age = age;
+    this.name = name;
+  }
+  sayHi(msg: string) {
+    console.log(msg);
+  }
+}
+let user1 = new Person(20, 'doe');
+console.log(user1);
